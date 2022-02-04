14959495e9b7cde6cb3211101c7cffb9e39ef335
Wolfgang Barth <wob@swobspace.net>
2022-02-04 14:04:38 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_123921) do
+ActiveRecord::Schema.define(version: 2022_02_04_130138) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "sn", null: false
