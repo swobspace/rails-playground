06b930e61c971a5b7306240c16fd6e1d1ab9bb02
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2022-02-10 15:26:18 +0100

    reduce space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,31 +2,31 @@ module ApplicationHelper
 
   def back_link
    link_to(raw(%Q[<i class="fas fa-arrow-left"></i>]), :back,
-            class: 'btn btn-outline-secondary me-2')
+            class: 'btn btn-outline-secondary me-1')
   end
 
   def new_link(obj)
     link_to(raw(%Q[<i class="fas fa-plus"></i>]), new_polymorphic_path(obj),
             target: '_top',
-            class: 'btn btn-outline-secondary me-2')
+            class: 'btn btn-outline-secondary me-1')
   end
 
   def show_link(obj)
     link_to(raw(%Q[<i class="fas fa-eye"></i>]), polymorphic_path(obj),
             target: '_top',
-            class: 'btn btn-outline-secondary me-2')
+            class: 'btn btn-outline-secondary me-1')
   end
 
   def edit_link(obj)
     link_to(raw(%Q[<i class="fas fa-pencil-alt"></i>]), edit_polymorphic_path(obj),
             target: '_top',
-            class: 'btn btn-outline-secondary me-2')
+            class: 'btn btn-outline-secondary me-1')
   end
 
   def delete_link(obj)
     button_to(raw(%Q[<i class="fas fa-trash-alt"></i>]), obj,
             method: :delete,
             target: '_top',
-            class: 'btn btn-danger me-2')
+            class: 'btn btn-danger me-1')
   end
 end
