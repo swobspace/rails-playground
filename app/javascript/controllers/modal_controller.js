33be655e94c759621005384ca86fc7c42433108d
Wolfgang Barth <wob@swobspace.net>
2022-02-26 11:58:09 +0100

    cleanup: close always on turbo:submit-end

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,40 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="modal"
 export default class extends Controller {
+
   // auto-open modal on connect
   connect() {
-    // console.log("modal#connect")
+    console.log("modal#connect")
     this.modal = new bootstrap.Modal(this.element)
     this.modal.show()
   }
 
   disconnect() {
-    // console.log("modal#disconnect")
-    this.close()
+    console.log("modal#disconnect")
   }
 
   close() {
-    // console.log("modal#close")
-    // close modal element
+    console.log("modal#close")
     this.modal.hide()
   }
 
-  /* close the modal form only if submission is successful
-     catch turbo:submit-end via action 
-     (don't include the controller here, modal controller is always
-      included via sharedModal)
-
-     <%= form_for(@mymodel,
-         data: { action: "turbo:submit-end->modal#handleSubmit" }) do |f| %>
-   */
-
+  // catch turbo:submit-end via action 
   handleSubmit = (event) => {
-    // console.log("modal#handleSubmit")
+    console.log("modal#handleSubmit")
     // close modal if form submission is successful
     if (event.detail.success) {
-      this.close()
+      console.log("modal#handleSubmit event successful")
     }
+    this.close()
   }
 
 }
