ff47f5c563f7919a524d8b27e8b67bf538e46a91
Wolfgang Barth <wob@swobspace.net>
2025-03-22 18:04:39 +0100

    refactor action_view helpers with :options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,29 +5,27 @@ module ApplicationHelper
             class: 'btn btn-outline-secondary me-1')
   end
 
-  def new_link(obj)
-    link_to(raw(%Q[<i class="fas fa-plus"></i>]), new_polymorphic_path(obj),
-            target: '_top',
-            class: 'btn btn-outline-secondary me-1')
+  def new_link(obj, opts = {})
+    options = {class: 'btn btn-outline-secondary me-1'}.merge(opts)
+    link_to(raw(%Q[<i class="fas fa-plus"></i>]), new_polymorphic_path(obj), options)
   end
 
-  def show_link(obj)
-    link_to(raw(%Q[<i class="fas fa-eye"></i>]), polymorphic_path(obj),
-            target: '_top',
-            class: 'btn btn-outline-secondary me-1')
+  def show_link(obj, opts = {})
+    options = {class: 'btn btn-outline-secondary me-1'}.merge(opts)
+    link_to(raw(%Q[<i class="fas fa-eye"></i>]), polymorphic_path(obj), options)
   end
 
-  def edit_link(obj)
-    link_to(raw(%Q[<i class="fas fa-pencil-alt"></i>]), edit_polymorphic_path(obj),
-            target: '_top',
-            class: 'btn btn-outline-secondary me-1')
+  def edit_link(obj, opts = {})
+    options = {class: 'btn btn-outline-secondary me-1'}.merge(opts)
+    link_to(raw(%Q[<i class="fas fa-pencil-alt"></i>]), 
+            edit_polymorphic_path(obj),
+            options)
   end
 
-  def delete_link(obj)
-    button_to(raw(%Q[<i class="fas fa-trash-alt"></i>]), obj,
-            method: :delete,
-            target: '_top',
-            form: { 'data-turbo-confirm': 'Are you sure?' },
-            class: 'btn btn-danger me-1')
+  def delete_link(obj, opts = {})
+    options = { method: :delete,
+                form: { 'data-turbo-confirm': 'Are you sure?' },
+                class: 'btn btn-danger me-1' }.merge(opts)
+    button_to(raw(%Q[<i class="fas fa-trash-alt"></i>]), obj, options)
   end
 end
